37f55b2a805ac4bad03e8d7b3758b97dc87d20c0
Sagar <sagar.0dev@gmail.com>
2022-04-14 17:24:18 +0530

    Lecture 1

---
+++ src/com/sagar/Lecture1.java
@@ -0,0 +1,14 @@
+package com.sagar;
+
+public class Lecture1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+/*Task:
+    Print:
+        My name is 'Your-Name'
+        2 + 3 = 5 (Here 5 can't be a string)
+  Solution:
+    By-
+*/
