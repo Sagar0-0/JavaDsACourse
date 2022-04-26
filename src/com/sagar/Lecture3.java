2c48825b9178e0d6c474496fb7ab46fc646bde9d
Sagar <sagar.0dev@gmail.com>
2022-04-26 16:21:59 +0530

    Lecture3

---
+++ src/com/sagar/Lecture3.java
@@ -0,0 +1,54 @@
+package com.sagar;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Lecture3 {
+    public static void main(String[] args){
+//        Scanner sc=new Scanner(System.in);
+//        int num=sc.nextInt();
+//        byte b=sc.nextByte();
+//        short s=sc.nextShort();
+//        long l=sc.nextLong();
+//        double db=sc.nextDouble();
+//        float ft=sc.nextFloat();
+//        String str=sc.next();
+//        System.out.println("Our string input was: "+str);
+
+//        InputStreamReader isr=new InputStreamReader(System.in);
+//        BufferedReader br=new BufferedReader(isr);
+//        double i=Double.parseDouble(br.readLine());
+//        System.out.println(i+1);
+//        int num=1;
+
+//        byte b=2;
+//        short s=b;
+//        int i=s;
+//        long l=i;
+
+//        int i=20;
+//        short s=(short)i;
+//        System.out.println(i);
+//        System.out.println(s);
+
+//        double db=2.345;
+//        int i=(int)db;
+//        System.out.println(db);
+//        System.out.println(i);
+//        double db2=i;
+//        System.out.println(db2);
+    }
+}
+/*
+Task 3:
+    > Using Scanner:
+        1. Take 2 'float' inputs
+        2. Add them +
+        3. Downcast them into 'short' and print
+    > Using BufferedReader:
+        1. Take 2 'Integer' inputs using "Integer.parseInt()"
+        2. Add them +
+        3. Downcast them into 'byte' and print
+ */
