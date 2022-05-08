6ca67debf4a24fe61008111a34d8bda7836cd185
Sagar <sagar.0dev@gmail.com>
2022-05-08 16:39:17 +0530

    Lecture5

---
+++ src/com/sagar/Lecture5.java
@@ -0,0 +1,27 @@
+package com.sagar;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class Lecture5 {
+    public static void main(String[] args) {
+//        if weekend
+//                sleep
+//        if working
+//            work
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        boolean bool= n<10 ? true : false;
+//        System.out.println(bool);
+//        System.out.println("Program ends");
+    }
+}
+/*
+Task 5:
+    1. Take 3 integer input 'a','b','c'
+    2. Print all of them in descending order.
+Solution:
+
+-By
+ */
