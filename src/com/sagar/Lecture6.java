088b7bde598704c3fa29654f7cace62f51cb23d1
Sagar <sagar.0dev@gmail.com>
2022-05-08 18:58:20 +0530

    Lecture6

---
+++ src/com/sagar/Lecture6.java
@@ -0,0 +1,42 @@
+package com.sagar;
+
+import java.util.Scanner;
+
+public class Lecture6 {
+    public static void main(String[] args) {
+//        ~
+//        int n=2;
+//        System.out.println(n);
+//        n=~n;
+//        System.out.println(n);
+//        <<
+//        int n=10;
+//        System.out.println(n);
+//        n=n<<1;
+//        System.out.println(n);
+//        >>
+//        int n=-6;
+//        System.out.println(n);
+//        n=n>>>1;
+//        System.out.println(n);
+        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        if((n&1)==1){
+//            System.out.println("ODD");
+//        }else{
+//            System.out.println("EVEN");
+//        }
+    }
+}
+
+/*
+Task 6:
+    1. Take one character input or
+       take single char string input and type cast it to char
+    2. Change the case of that character (a -> A)/(A -> a)
+       using only bitwise operator.
+    3. Print out the resulting character.
+
+Solution:
+-By
+ */
