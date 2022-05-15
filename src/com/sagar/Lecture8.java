0e5741b81f0a9572c2f545a7ee9b320fbe90f751
Sagar <sagar.0dev@gmail.com>
2022-05-15 18:07:37 +0530

    Lecture6

---
+++ src/com/sagar/Lecture8.java
@@ -0,0 +1,104 @@
+package com.sagar;
+
+import java.util.Scanner;
+
+public class Lecture8 {
+    public static void main(String[] args) {
+        int numberOfDay = 4;
+//        if(numberOfDay==1){
+//            System.out.println("Monday");
+//        }
+//        else if(numberOfDay==2){
+//            System.out.println("Tuesday");
+//        }
+//        else if(numberOfDay==3){
+//            System.out.println("Wednesday");
+//        }
+//        else if(numberOfDay==4){
+//            System.out.println("Thursday");
+//        }
+//        else if(numberOfDay==5){
+//            System.out.println("Friday");
+//        }
+//        else if(numberOfDay==6){
+//            System.out.println("Saturday");
+//        }
+//        else if(numberOfDay==7){
+//            System.out.println("Sunday");
+//        }else{
+//            System.out.println("Wrong number");
+//        }
+//        switch(numberOfDay){
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//                break;
+//            case 5:
+//                System.out.println("Friday");
+//                break;
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//            case 7:
+//                System.out.println("Sunday");
+//                break;
+//            default:
+//                System.out.println("Wrong number");
+//                break;
+//        }
+//        Scanner sc=new Scanner((System.in));
+//        int a=sc.nextInt();
+//        int b=sc.nextInt();
+//        char operator=sc.next().charAt(0);
+//        switch (operator){
+//            case '+':
+//                System.out.println(a+b);
+//                break;
+//            case '-':
+//                System.out.println(a-b);
+//                break;
+//            case '*':
+//                System.out.println(a*b);
+//                break;
+//            case '/':
+//                System.out.println(a/b);
+//                break;
+//            default:
+//                System.out.println("Invalid operator");
+//        }
+
+//        int n = 200;
+//        switch (n) {
+//            case 10:
+//            case 20:
+//                System.out.println("n is smaller");
+//                break;
+//            case 50:
+//                System.out.println("n is 50");
+//                break;
+//            case 100:
+//                System.out.println("n is large");
+//                break;
+//            default:
+//                System.out.println("i don't know");
+//        }
+    }
+}
+/*
+Task 8:
+    1. Take two integer inputs a and b
+    2. use switch with a
+    3. if a is 10 then use nested switch with b
+    4. if b is also 10 then print they are same
+    5. if b is 20 then print b is greater than a
+    6. if a is 20 then print a wins.
+ */
+
