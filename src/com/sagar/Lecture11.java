de05e9137e470f6278e3be5dfbcdae4fcb17b381
Sagar <sagar.0dev@gmail.com>
2022-06-10 15:26:25 +0530

    Lecture11

---
+++ src/com/sagar/Lecture11.java
@@ -0,0 +1,36 @@
+package com.sagar;
+
+import java.util.Scanner;
+
+public class Lecture11 {
+    public static void main(String[] args) {
+//        statement 1(initialisation)
+//        while(statement2 - condition){
+//              your code goes here...
+//              re-initialisation
+//        }
+
+        Scanner sc=new Scanner(System.in);
+        int num=1;
+//        while(true){
+//            System.out.println(num);
+//            num++;
+//            if(num==100)break;
+//        }
+//        while(num<=10){
+//            if(num>=4 && num<=8){
+//                num++;
+//                continue;
+//            }
+//            System.out.println(num);
+//            num++;
+//        }
+    }
+}
+/*
+Task 11:
+
+    1. Factorial
+    2. Fibonacci
+    3. Prime or Not?
+ */
