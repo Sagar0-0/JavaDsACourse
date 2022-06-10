fbba6732a85da8e42fe0b44db3291ca4e1f8ebf9
Sagar <sagar.0dev@gmail.com>
2022-06-10 18:27:10 +0530

    Lecture12

---
+++ src/com/sagar/Lecture12.java
@@ -0,0 +1,87 @@
+package com.sagar;
+
+import java.util.Scanner;
+
+public class Lecture12 {
+    public static void main(String[] args) {
+//        int num=1;
+//        do{
+//            System.out.println(num);
+//            num++;
+//        }while(num<0);
+
+//        find maximum digit of a number
+//        15349
+        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        if(n<0)n=-n;
+//        int maximum=0;
+//        while(n>0){
+//            int currDigit=n%10;
+//            if(currDigit>maximum)maximum=currDigit;
+//            n=n/10;
+//        }
+//        System.out.println(maximum);
+
+        //sum of all digits
+//        int sum=0;
+//        while(n>0){
+//            int currDigit=n%10;
+//            sum+=currDigit;
+//            n/=10;
+//        }
+//        System.out.println(sum);
+
+        //product of all digits
+
+//        int product=1;
+//        while(n>0){
+//            int currDigit=n%10;
+//            if(currDigit==0){
+//                n/=10;
+//                continue;
+//            }
+//            product*=currDigit;
+//            n/=10;
+//        }
+//        System.out.println(product);
+
+//        occurance of digits
+//        2202
+//        ans = 3;
+        int n=sc.nextInt();
+        if(n<0)n=-n;
+//        int x=sc.nextInt();
+//        if(x<0)x=-x;
+//        int ans=0;
+//
+//        while(n>0){
+//            int currDigit=n%10;
+//            if(currDigit==x){
+//                ans++;
+//            }
+//            n=n/10;
+//        }
+//        System.out.println(ans);
+
+//        reverse a number
+//        123 - 321
+//        ans=3
+
+//        int ans=0;
+//        while(n>0){
+//            int currDigit=n%10;
+//            ans=ans*10 +currDigit;
+//            n=n/10;
+//        }
+//        System.out.println(ans);
+
+    }
+}
+/*
+Task 12:
+    ** ONLY USE FOR LOOP **
+    1. Find max and min digit using only one loop
+    2. Reverse a negative number(-123 -> -321)
+    3. (Option) Practice all problems
+ */
