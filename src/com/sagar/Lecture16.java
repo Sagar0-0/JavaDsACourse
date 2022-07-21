b31d2a0f7f39e8f88dffdb09f5666f220e418361
Sagar <sagar.0dev@gmail.com>
2022-07-21 22:21:00 +0530

    Lecture16

---
+++ src/com/sagar/Lecture16.java
@@ -0,0 +1,15 @@
+package com.sagar;
+
+import java.util.Scanner;
+
+public class Lecture16 {
+    public static void main(String[] args) {
+        int a=0;
+        int arr[]=new int[5];
+    }
+}
+/*
+Task 16:
+    1. Initialise one array and take all value from input
+    2. Create a clone of that array into a new array.
+ */
