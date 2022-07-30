765ea8dd11a298cbed1c7dd2c85a1dbc6f7462b3
Sagar <sagar.0dev@gmail.com>
2022-07-30 18:25:36 +0530

    Lecture18

---
+++ src/com/sagar/Lecture18.java
@@ -0,0 +1,93 @@
+package com.sagar;
+
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class Lecture18 {
+    public static void main(String[] args) {
+//        int[] arr=new int[3];
+//        int[][] matrix=new int[3][2];
+
+//        matrix[0][1]=23;
+//        matrix[1][1]=34;
+//        matrix[0][0]=2;
+////        for(int i=0;i<matrix.length;i++){
+////            for(int j=0;j<matrix[i].length;j++){
+////                System.out.print(matrix[i][j]+" ");
+////            }
+////            System.out.println();
+////        }
+//
+//        for(int[]arr:matrix){
+//            for(int i:arr){
+//                System.out.print(i+" ");
+//            }
+//            System.out.println();
+//        }
+
+//        Scanner sc=new Scanner(System.in);
+//        //rows
+//        //cols
+//        //elements=rows*cols
+//        System.out.println("Enter rows:");
+//        int rows=sc.nextInt();
+//        System.out.println("Enter cols: ");
+//        int cols=sc.nextInt();
+//
+//        int[][] a=new int[rows][cols];
+//
+//        System.out.println("Enter "+ rows*cols +" values for a");
+//        for(int row=0;row<a.length;row++){
+//            for(int col=0;col<a[row].length;col++){
+//                a[row][col]=sc.nextInt();
+//            }
+//        }
+//
+//        System.out.println("Matrix a is:");
+//        for(int[]arr:a){
+//            for(int i:arr){
+//                System.out.print(i+" ");
+//            }
+//            System.out.println();
+//        }
+//
+//
+//        //b matrix
+//        int[][] b=new int[rows][cols];
+//
+//        System.out.println("Enter "+ rows*cols +" values for b");
+//        for(int row=0;row<b.length;row++){
+//            for(int col=0;col<b[row].length;col++){
+//                b[row][col]=sc.nextInt();
+//            }
+//        }
+//
+//        System.out.println("Matrix b is:");
+//        for(int[]arr:b){
+//            for(int i:arr){
+//                System.out.print(i+" ");
+//            }
+//            System.out.println();
+//        }
+//
+//        //adding a and b
+//        int[][]ans=new int[rows][cols];
+//        for(int i=0;i<rows;i++){
+//            for(int j=0;j<cols;j++){
+//                ans[i][j]=a[i][j]+b[i][j];
+//            }
+//        }
+//
+//        System.out.println("Answer matrix:");
+//        for(int[]arr:ans){
+//            for(int i:arr){
+//                System.out.print(i+" ");
+//            }
+//            System.out.println();
+//        }
+    }
+}
+/*
+Task 18:
+    1. Use a while loop to take input and traverse a matrix
+ */
