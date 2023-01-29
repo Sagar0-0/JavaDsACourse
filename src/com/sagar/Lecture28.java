cce7cae04127f149f743c152a20f343077ca6aba
Sagar <sagar.0dev@gmail.com>
2023-01-29 16:02:24 +0530

    Lecture28

---
+++ src/com/sagar/Lecture28.java
@@ -0,0 +1,16 @@
+package com.sagar;
+
+public class Lecture28 {
+    int num;
+    public static void main(String[] args) {
+        /*
+        Public - everywhere (same + diff packages)
+        Protected - Within same packages + in subclass of other packages
+        Default - Within same package
+        Private - Within same class
+         */
+    }
+    static class MyClass{
+        protected int value;
+    }
+}
